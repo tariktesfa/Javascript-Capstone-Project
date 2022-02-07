8afd1b8a90a07af22abb96a6b5d2d44cb3201a58
tariktesfa <tariktesfa9090@gmail.com>
2022-02-07 17:51:15 +0300

    Removed html loader

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
     ],
   },
   mode: 'development',
