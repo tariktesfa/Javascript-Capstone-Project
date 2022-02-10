7b2a4877d09d72ef3af9bb3d9f3cbc1723ad60e8
tariktesfa <tariktesfa9090@gmail.com>
2022-02-10 12:22:27 +0300

    Remove dist folder

---
+++ src/test/commentCounter.test.js
@@ -0,0 +1,32 @@
+/**
+ * @jest-environment jsdom
+ */
+import { commentCounter } from '../modules/commentCounter.js';
+
+describe('Testing comment counter', () => {
+  const commentArr = [
+    {
+      id: 1,
+      name: 'Tarik',
+      comment: 'I loved it',
+    },
+    {
+      id: 12,
+      username: 'Josphat',
+      comment: 'Great movie ever',
+    },
+    {
+      id: 19,
+      username: 'Tarikwa',
+      comment: 'Not interesting',
+    },
+  ];
+  const counter = commentCounter(commentArr);
+  expect(counter).toBe(3);
+});
+
+test('Testing the number of comments is 3', () => {
+  const arr = ['Amzing movie', 'Never watch such Movie', 'Very annoying'];
+  const counter = commentCounter(arr);
+  expect(counter).toBe(3);
+});
